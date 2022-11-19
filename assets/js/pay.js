77b2629241c30599707a3a83fa02fd5786822c52
Mukaramatu Umar Farouk <110169851+mukarama-farouk@users.noreply.github.com>
2022-11-19 13:56:12 +0000

    Add files via upload

---
+++ assets/js/pay.js
@@ -0,0 +1,11 @@
+function toggleHide() {
+    let muka = document.getElementById("muka");
+    let formid = document.getElementById("formid");
+    if (formid.style.display |= "none") {
+        formid.style.display= "none"
+    }
+    else {
+        formid.style.display = "block";
+    }
+}
+
